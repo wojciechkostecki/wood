7ab9968e8cb1e5007e98c3382f216486a65b208d
48531 <wt.kostecki@gmail.com>
2021-09-16 09:55:40 +0200

    Initial commit

---
+++ src/test/java/pl/wojciechkostecki/wood/WoodApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wojciechkostecki.wood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
